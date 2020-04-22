a281253040ea4c683084648af3f50a24153bc41a
Didar <korrega_finance@mail.ru>
2020-04-22 16:16:03 +0600

    This is first commit with 3 tries

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+'use esversion: 7';
+
+let answer = + prompt("Are you 18+?", "yes");
+
+console.log(typeof(answer));
+
+console.log("arr" + " - object");
+
+console.log(4 + " - object");
+
+console.log(3**2);
